72f6af4d85e0e148e56ed8419c325011b2973b2f
Goksel Aktas <aktas_goksel@hotmail.com>
2024-03-06 13:16:46 +0000

    If Else Statements

---
+++ 7 day of JavaScript/Day1-03.js
@@ -0,0 +1,27 @@
+//Do not declare variable marks.
+//Write your code below this line.
+//Do not declare the variable marks. It is declared inside our code checker.
+//Use console.log for printing statements to the console.
+
+if (marks > 90){
+    console.log("AA")
+}
+else if (marks <= 90 & marks > 80){
+    console.log("AB")
+}
+else if (marks <= 80 & marks > 70){
+    console.log("BB")
+}
+else if (marks <= 70 & marks > 60){
+    console.log("BC")
+}
+else if (marks <= 60 & marks > 50){
+    console.log("CC")
+}
+else if (marks <= 50 & marks > 40){
+    console.log("CD")
+}
+else if (marks <= 40 & marks > 30){
+    console.log("DD")
+}
+else { console.log("FF")}
